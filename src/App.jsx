36c6ad2c65fad024a3a6705d09f1dc0331676c7b
gih1teixeira <gihapt@gmail.com>
2026-08-24 16:02:42 -0300

    Projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+
+import {useState, useEffect} from 'react'
+function App() {
+  //Crie duas const para guardar e carregar tarefas..
+  const [tarefas, setTarefas] = useState([])
+  const [carregando, setCarregando] = useState(true)
+  //useEffect com fetch (requisições assincronas)
+  useEffect(() => {
+    fetch('http://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then((resposta) => resposta.json())
+    .then((dados) => {
+      setTarefas(dados); //Salva os dados vindos da API no estado
+      setCarregando(false) //Desativa a mensagem carregando
+    })
+
+  }, []); // Array vazia para executar apenas UMA vez ao abrir a tela.
+  return (
+    <>
+      <div>
+        <div>
+          <h2>Tarefas vindas da API</h2>
+          <p>Consumindo dados de JSONPLACEHOLDERS via fetch e useEffect</p>
+          {carregando ? (
+            <div>
+              Carregando...
+            </div>
+          ):(
+            <ul>
+              {tarefas.map((item) => (
+                <li key={item.id}>{item.title}{item.completed ? 'Concluído' : 'Pendente'}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </>
+  )
+}
+ 
+export default App
